50c99b72341982089fd7d00bda1cab93753bd1de
Elijah <awkwardcheesecake@Gmail.com>
2013-09-18 12:05:50 -0500

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,123 +1,221 @@
-// Create the canvas
+// BEST COMMENT EVER
+
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
 canvas.width = 512;
 canvas.height = 480;
 document.body.appendChild(canvas);
 
-// Background image
+
+//bg img
+
 var bgReady = false;
-var bgImage = new Image();
-bgImage.onload = function () {
+var bgImg = new Image();
+bgImg.onload = function () {
 	bgReady = true;
 };
-bgImage.src = "images/background.png";
+bgImg.src = "background.png";
 
-// Hero image
-var heroReady = false;
-var heroImage = new Image();
-heroImage.onload = function () {
-	heroReady = true;
+//player
+var playerImgReady = false;
+var playerImg = new Image();
+playerImg.onload = function () {
+	playerImgReady = true;
 };
-heroImage.src = "images/hero.png";
+playerImg.src = "hero.png";
 
-// Monster image
-var monsterReady = false;
-var monsterImage = new Image();
-monsterImage.onload = function () {
-	monsterReady = true;
+//Mob
+var mobImgReady = false;
+var mobImg = new Image();
+mobImg.onload = function() {
+	mobImgReady = true;
 };
-monsterImage.src = "images/monster.png";
+mobImg.src = "monster.png";
 
-// Game objects
-var hero = {
-	speed: 256 // movement in pixels per second
+// Objects
+function GameImage(file) {
+	this.img = new Image();
+	this.img.isReady = false;
+	this.ready = false;
+	this.img.onload = function() {
+		this.isReady = true;
+	};
+
+	this.img.src = file;
+}
+
+GameImage.prototype.Draw = function(x, y){
+
+	if(this.img.isReady) {
+		ctx.drawImage(this.img, x, y);
+	}
+}
+
+function Player(s, image) {
+	this.speed = s;
+	this.i = image;
+	this.x = 0;
+	this.y = 0;
+
+	this.upKey = 38;
+	this.downKey = 40;
+	this.leftKey = 37;
+	this.rightKey = 39;
+}
+
+Player.prototype.Draw = function(){
+	this.i.Draw(this.x, this.y);
+}
+
+
+Player.prototype.Move = function(mod){
+	if (this.upKey in keysDown) { // player holding up
+		if(this.y > 32){
+			this.y -= this.speed * mod;
+		}
+	}
+	if (this.downKey in keysDown) { // player holding down
+		if(this.y < canvas.height - 32 - 32){
+			this.y += this.speed * mod;
+		}
+	}
+	if (this.leftKey in keysDown) { // player holding left
+		if(this.x > 32){
+			this.x -= this.speed * mod;
+		}
+	}
+	if (this.rightKey in keysDown) { // player holding right
+		if(this.x < canvas.width - 32 - 32){
+			this.x += this.speed * mod;
+		}
+	}
 };
-var monster = {};
+
+// TEST
+testImage = new GameImage("hero.png");
+PC = new Player(512, testImage);
+PC.x = 50;
+PC.y = 50;
+
+
+//Vars
+
+var player = {
+	speed: 256, // movement in pixels per second
+	width: 32,
+	height: 32,
+	x: 50,
+	y: 50
+};
+
+//var player = new Player(256, playerImg)
+
+var mob = {
+	speed: 128,
+	x: 0,
+	y: 0,
+	moving: false
+};
+
 var monstersCaught = 0;
 
-// Handle keyboard controls
+// Keyboard handlers
 var keysDown = {};
 
-addEventListener("keydown", function (e) {
+addEventListener("keydown", function(e){
 	keysDown[e.keyCode] = true;
 }, false);
 
-addEventListener("keyup", function (e) {
+addEventListener("keyup", function(e){
 	delete keysDown[e.keyCode];
 }, false);
 
-// Reset the game when the player catches a monster
-var reset = function () {
-	hero.x = canvas.width / 2;
-	hero.y = canvas.height / 2;
+// reset game
+var reset = function() {
+	player.x = canvas.width / 2;
+	player.y = canvas.width / 2;
 
-	// Throw the monster somewhere on the screen randomly
-	monster.x = 32 + (Math.random() * (canvas.width - 64));
-	monster.y = 32 + (Math.random() * (canvas.height - 64));
+	mob.x = 32 + (Math.random() * (canvas.width - 96));
+	mob.y = 32 + (Math.random() * (canvas.height - 96));
 };
 
-// Update game objects
-var update = function (modifier) {
-	if (38 in keysDown) { // Player holding up
-		hero.y -= hero.speed * modifier;
+
+//Update
+var update = function(modifier){
+	if (38 in keysDown) { // player holding up
+		if(player.y > 32){
+			player.y -= player.speed * modifier;
+		}
 	}
-	if (40 in keysDown) { // Player holding down
-		hero.y += hero.speed * modifier;
+	if (40 in keysDown) { // player holding down
+		if(player.y < canvas.height - 32 - 32){
+			player.y += player.speed * modifier;
+		}
 	}
-	if (37 in keysDown) { // Player holding left
-		hero.x -= hero.speed * modifier;
+	if (37 in keysDown) { // player holding left
+		if(player.x > 32){
+			player.x -= player.speed * modifier;
+		}
 	}
-	if (39 in keysDown) { // Player holding right
-		hero.x += hero.speed * modifier;
+	if (39 in keysDown) { // player holding right
+		if(player.x < canvas.width - 32 - 32){
+			player.x += player.speed * modifier;
+		}
 	}
 
-	// Are they touching?
+	// Check for collision
 	if (
-		hero.x <= (monster.x + 32)
-		&& monster.x <= (hero.x + 32)
-		&& hero.y <= (monster.y + 32)
-		&& monster.y <= (hero.y + 32)
+		player.x <= (mob.x + 32)
+		&& mob.x <= (player.x + 32)
+		&& player.y <= (mob.y + 32)
+		&& mob.y <= (player.y + 32)
 	) {
 		++monstersCaught;
 		reset();
 	}
 };
 
-// Draw everything
-var render = function () {
-	if (bgReady) {
-		ctx.drawImage(bgImage, 0, 0);
+//Drawing!
+var render = function(){
+	if(bgReady) {
+		ctx.drawImage(bgImg, 0, 0);
 	}
 
-	if (heroReady) {
-		ctx.drawImage(heroImage, hero.x, hero.y);
+	if(playerImgReady) {
+		//ctx.drawImage(playerImg, player.x, player.y);
 	}
 
-	if (monsterReady) {
-		ctx.drawImage(monsterImage, monster.x, monster.y);
+	if(PC.i.img.isReady) {
+		PC.Draw();
 	}
 
-	// Score
+	if(mobImgReady) {
+		ctx.drawImage(mobImg, mob.x, mob.y);
+	}
+
+	// score
 	ctx.fillStyle = "rgb(250, 250, 250)";
 	ctx.font = "24px Helvetica";
 	ctx.textAlign = "left";
 	ctx.textBaseline = "top";
-	ctx.fillText("Goblins caught: " + monstersCaught, 32, 32);
+	ctx.fillText("Mobs Caught: " + monstersCaught, 32, 32);
 };
 
-// The main game loop
-var main = function () {
+// MAIN LOOP
+var main = function() {
 	var now = Date.now();
-	var delta = now - then;
-
-	update(delta / 1000);
+	var delta = now-then;
+	
+	PC.Move(delta/1000);
 	render();
 
-	then = now;
+	then = now;	
 };
 
-// Let's play this game!
 reset();
 var then = Date.now();
-setInterval(main, 1); // Execute as fast as possible
+setInterval(main, 1); //execute as fast as possible
+
+//alert(bgReady);
+
+
